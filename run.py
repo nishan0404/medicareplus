5c0f5d0a830d29cf2e59546d6f7d450bf5be5c04
Nishan Bhattarai <nishanbhattarai6789@gmail.com>
2026-04-08 19:30:06 +1000

    fix db.create_all for production

--- run.py
+++ run.py
@@ -1,19 +1,20 @@
 from dotenv import load_dotenv
 load_dotenv()
-
 from app import create_app, db, socketio
 from app.models import (
     Patient, PatientProfile, PasswordResetToken,
     Appointment, Cancellation, DoctorAvailability, EmailLog,
     Doctor, ConsultationNote, Prescription,
     Admin, SymptomLog, Condition, AuditLog,
-    CallSession                              # ← NEW: import CallSession
+    CallSession,
+    ChatMessage, ChatAttachment,
 )
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+    print("✅ All database tables created!")
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        print("✅ All database tables created!")
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)  # ← CHANGED: socketio.run instead of app.run
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
